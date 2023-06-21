7d734b78c15641c85e195cd39fe8b5e1de5aa983
araguma <itetbitb@gmail.com>
2023-06-21 15:50:53 -0400

    Switch to modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = 3000;
 const app = express();
